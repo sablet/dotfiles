4e95ac123c99cd72b440375b7fa07d3f7d16c507
sablet <e.typ719@gmail.com>
2016-09-07 15:31:36 +0900

    remove files

--- .ubuntu-dotfiles/mozilla/firefox/vp8wtdpo.default/extensions/staged/{0b457cAA-602d-484a-8fe7-c1d894a011ba}/defaults/preferences/prefs.js
+++
@@ -1,2 +0,0 @@
-pref("extensions.{0b457cAA-602d-484a-8fe7-c1d894a011ba}.description", "chrome://fireshot/locale/fireshot.properties");
-pref("extensions.{0b457cAA-602d-484a-8fe7-c1d894a011ba}.translator", "chrome://fireshot/locale/fireshot.properties");
