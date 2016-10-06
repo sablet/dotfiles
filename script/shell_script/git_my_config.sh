3d472dc97102acb05bfc6eb0861d416dc6dec341
sablet <e.typ719@gmail.com>
2016-10-06 09:13:29 +0900

    git config

--- script/shell_script/git_my_config.sh
+++ script/shell_script/git_my_config.sh
@@ -1,3 +1,6 @@
 git config --global user.name "sablet"
 git config --global user.email "e.typ719@gmail.com"
 git config --global color.ui auto
+
+git config --global http.proxy $http_proxy
+git config --global https.proxy $https_proxy
